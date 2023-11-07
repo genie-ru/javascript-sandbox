e14644c1ef47d814e071b2e632ad1d1b37287acf
genie-ru <genie.zgmf987@gmail.com>
2023-11-08 03:44:55 +0900

    test

---
+++ 01-varialbles-data-types/03-variables/script.js
@@ -0,0 +1,47 @@
+// Ways to decleare a variables
+// var, let, const
+
+let firstName = 'John';
+let lastName = 'Doe';
+
+console.log(firstName, lastName);
+
+let age = 30;
+console.log(age);
+let score;
+score = 1;
+
+age = 31;
+console.log(age);
+
+if (true) {
+    score = score + 1;
+}
+
+console.log(score);
+
+const x = 100;
+
+// x = 200;
+
+console.log(x);
+
+const arr = [1, 2, 3, 4];
+arr.push(5);
+console.log(arr);
+
+const person = {
+    name : 'Brad',
+};
+
+person.name = 'John';
+
+person.email = 'brad@gmail.com';
+console.log(person);
+
+// Declare multiple values at once
+let a, b, c;
+
+const d = 10, e = 20, f = 30;
+console.log(d);
+console.log(f);
