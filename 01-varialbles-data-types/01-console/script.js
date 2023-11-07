9e2be7f55ca81c6df7ffde2a0f561e8070f36d7b
genie-ru <genie.zgmf987@gmail.com>
2023-11-08 03:26:11 +0900

    test

--- 01-varialbles-data-types/01-console/script.js
+++ 01-varialbles-data-types/01-console/script.js
@@ -1,1 +1,26 @@
-alert('console.log');
+let a = 'test';
+console.log('test');
+console.log(a);
+let data = new Date();
+console.log(data);
+console.log(100);
+console.log('Hello World');
+console.log(20, 'Hello', true);
+
+const x = 100;
+
+console.log(x);
+console.log(typeof x);
+console.error('Alert');
+
+console.warn('Warning');
+console.table({name: 'Brad', email: 'brad@gmail.com'});
+
+console.group('simple');
+console.log(x);
+console.error('Aleart');
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white; color: green';
+console.log('%c Hello World', styles);
